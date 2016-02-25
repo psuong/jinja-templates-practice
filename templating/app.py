70aa3c31a3b025245bd450dea3df36d21b907d33
Porrith Suong <psuong95@gmail.com>
2016-02-25 11:02:21 -0500

    Adds if statements to the Jinja templates.

--- templating/app.py
+++ templating/app.py
@@ -6,7 +6,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('extends_example.html', name='My Name', description='My description')
+    # Directly assign the variables in the return statement when rendering the page.
+    # Remember that you want to render the HTML file that is extending from the parent HTML file.
+    pass
+
+@app.route('/another-link')
+def another_link():
+    # Create a dictionary and use Python's keyword arguments to assign and pass all the variables to the template.
+    pass
 
 
 if __name__ == '__main__':
